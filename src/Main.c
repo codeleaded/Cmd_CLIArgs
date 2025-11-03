85053ad8711cccf6561fddb2523a0a24ade2ca80
codeleaded <codeleaded@gmail.com>
2025-11-03 16:25:36 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,10 @@
+#if defined __linux__
 #include "/home/codeleaded/System/Static/Library/CLIArgs.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/CLIArgs.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/CLIArgs.h"
+#endif
 
 #define CLIARG_VERSION              (CLIARG_START + 0)
 #define CLIARG_LINKING              (CLIARG_START + 1)
